d68c87aa05cc9326e0159bd7a9147aab2cbb734f
Thomas Antoine <t.antoine@uclouvain.be>
2026-03-22 00:14:52 +0100

    Eliminate ordering by splitting activity and exchanges

--- src/acts/custom_activities.py
+++ src/acts/custom_activities.py
@@ -19,35 +19,6 @@ def load_custom_activities(yaml_path):
 
     return activities
 
-def order_activities(acts):
-    node_map = {n["id"]: n for n in acts}
-    present = set(node_map.keys())
-
-    visited = set()
-    result = []
-
-    def dfs(node):
-        if node["id"] in visited:
-            return
-
-        visited.add(node["id"])
-
-        for child in node["inputs"].values():
-            if "act_name" not in child:
-                #Smart activities can only link to ecoinvent at the moment
-                #Should be fixed if smart activities are extended further
-                continue
-            child_id = child["act_name"]
-            if child_id in present:
-                dfs(node_map[child_id])
-
-        result.append(node)
-
-    for act in acts:
-        dfs(act)
-
-    return result
-
 def input_to_activity(param_name, input_value, db):
     if "type" in input_value:
         input_value = smart_activity(input_value)
@@ -71,26 +42,32 @@ def input_to_activity(param_name, input_value, db):
     return activity, param
 
 def create_custom_activities(activities, foreground_db):
+    ret = []
     for activity in activities:
-        unit = activity["output"]["amount"]["unit"]
+        # Create new custom activity
+        act = agb.newActivity(
+            foreground_db,
+            activity['id'],
+            unit = activity["output"]["amount"]["unit"],
+            exchanges={}
+        )
+        ret.append((act,activity.get("inputs", [])))
+    return ret
+
+def add_all_exchanges(all_acts, foreground_db):
+    for act, input_data in all_acts:
         exchanges = {}
 
-        for input_name, input_value in activity.get("inputs", []).items():
-            param_name = f"{activity['id']}_{input_name}"
+        for input_name, input_value in input_data.items():
+            param_name = f"{act['name']}_{input_name}"
 
             child_act, param = input_to_activity(param_name, input_value, foreground_db)
             #Need to do the get in case where multiple inputs link to the same activity
             exchanges[child_act] =  exchanges.get(child_act,0) + param
 
-        # Create new custom activity
-        agb.newActivity(
-            foreground_db,
-            activity['id'],
-            unit,
-            exchanges=exchanges
-        )
+        act.addExchanges(exchanges)
 
 def generate_activities(path, db):
     custom_activities = load_custom_activities(path)
-    custom_activities = order_activities(custom_activities)
-    create_custom_activities(custom_activities, foreground_db=db)
+    acts = create_custom_activities(custom_activities, foreground_db=db)
+    add_all_exchanges(acts, foreground_db=db)
