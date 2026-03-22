66b2bc7fb34c249c561f6fbc7cabae8a3f2da599
Thomas Antoine <t.antoine@uclouvain.be>
2026-03-22 01:19:56 +0100

    Do not MC over unused variables

--- src/impacts/monte_carlo.py
+++ src/impacts/monte_carlo.py
@@ -1,5 +1,40 @@
 import lca_algebraic as agb
+import sympy as sp
 
-def stoch_impacts(ref_flow, impacts):
-    problem, params, Y = agb.stats._stochastics(ref_flow[0], impacts)
+def find_unused_params(root_act, db):
+    visited = set()
+    used_params = set()
+
+    def traverse(act):
+        if act in visited or act._data['database'] != db:
+            return
+        visited.add(act)
+
+        for exch_name, target_act, amount in act.listExchanges():
+            if isinstance(amount, sp.Expr):
+                for sym in amount.free_symbols:
+                    used_params.add(sym.name)
+
+            if target_act is not None:
+                traverse(target_act)
+
+    traverse(root_act)
+
+    all_params = agb.params.all_params()
+
+    unused = {
+        param.name: param.default
+        for param in all_params.values()
+        if param.name not in used_params
+    }
+
+    return unused
+
+
+def stoch_impacts(ref_flow, impacts, db):
+    # Fix unused params to avoid computation cost (if there is any?)
+    # If there is no time saved by fixing them beforehand,
+    # we could also just remove the columns 
+    unused = find_unused_params(ref_flow[0], db)
+    problem, params, Y = agb.stats._stochastics(ref_flow[0], impacts, **unused)
     return Y
